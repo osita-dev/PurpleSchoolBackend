57b9b9d2a6cfd54073f78f2a41773e132f206a8c
Christopher osita <70627592+osita-dev@users.noreply.github.com>
2025-11-12 05:16:33 +0100

    Update CORS origin to production URL

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173', 
+  origin: 'https://purpleschool.vercel.app', 
   credentials: true,
 }));
 app.use(express.json());
